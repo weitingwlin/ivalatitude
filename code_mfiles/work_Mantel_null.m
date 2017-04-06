c86e2fcdf76f7a889f5a024c7e2116e91193ce3d
wildwolflin <weiting.lin.lin@gmail.com>
2017-04-06 13:35:00 -0500

    Edit: mantel_null plot

--- code_mfiles/work_Mantel_null.m
+++ code_mfiles/work_Mantel_null.m
@@ -4,7 +4,7 @@ work_compile3
    
  %% simulation of null model
  rng(1)
- it = 10000; %Elapsed time is 9318.111275 seconds for 10000 iteration with parfor
+ it = 1; %Elapsed time is 9318.111275 seconds for 10000 iteration with parfor
  nullrlat = zeros(1, it);
   nullplat = zeros(1, it);
    nullrmon = zeros(1, it);
@@ -22,19 +22,23 @@ work_compile3
   nullrmon(i) =r2;  nullpmon(i) =p2;
  end
  toc
-save Mantel_null nullrlat nullrmon nullplat nullpmon
+% save Mantel_null nullrlat nullrmon nullplat nullpmon
  %% Mantel test: latitude
+ load Mantel_null 
+ it = 10000;
  rng(1)
  figure
  [r,p] = f_mantel(squareform(pdist(log(g_matST+1))),  squareform(pdist([latitudeST ])), 0, 10000);
  mysubplot(1,2,1)
-   histogram(nullrlat); hold on
+   h = histogram(nullrlat, 20); hold on
+   h.EdgeColor = 'w';
         q1 = quantile(nullrlat, [ 0.5 0.95],2);
         vline(q1(1), 7, '--');   vline(q1(2), 7, ':'); 
         vline( r, 2);
         title('Mantel r')
  mysubplot(1,2,2)
-   histogram(nullplat); hold on
+   h = histogram(nullplat, 20); hold on
+     h.EdgeColor = 'w';
         q1 = quantile(nullplat, [ 0.5 0.95],2);
         vline(q1(1), 7, '--');   vline(q1(2), 7, ':'); 
         vline( p, 2);
@@ -47,17 +51,49 @@ save Mantel_null nullrlat nullrmon nullplat nullpmon
  figure
  [r2,p2] = f_mantel(squareform(pdist(log(g_matST+1))),  squareform(pdist(t_infoST(:,2))), 0, 10000);
  mysubplot(1,2,1)
-   histogram(nullrmon); hold on
+   h = histogram(nullrmo, 20); hold on
+      h.EdgeColor = 'w';
         q1 = quantile(nullrmon, [ 0.5 0.95],2)
         %vline(q1(1), 7, ':'); 
         vline(q1(1), 7, '--');   vline(q1(2), 7, ':'); 
         vline( r2, 2);
         title('Mantel r')
  mysubplot(1,2,2)
-   histogram(nullpmon); hold on
+   h = histogram(nullpmon, 20); hold on
+     h.EdgeColor = 'w';
         q1 = quantile(nullpmon, [ 0.5 0.95],2);
         vline(q1(1), 7, '--');   vline(q1(2), 7, ':'); 
         vline( p2, 2);
         title('Mantel p')  
                    pctile_r =  (sum(nullrmon<r2)+sum(nullrmon ==r2)*0.5)/it;
-         mysubplot(1,2,0,{['Mantel test with monthe (' num2str(it) ') randomization'], ['percentile = ' num2str(pctile_r)]})     
+     
+%% in one plot
+ load Mantel_null 
+ it = 10000;
+ rng(1)
+ figure
+ 
+ mysubplot(1,2,1)
+  [r,p] = f_mantel(squareform(pdist(log(g_matST+1))),  squareform(pdist([latitudeST ])), 0, 10000);
+   h = histogram(nullrlat, 20); hold on
+   h.EdgeColor = 'w';
+        q1 = quantile(nullrlat, [ 0.5 0.95],2);
+        vline(q1(1), 7, '--');   vline(q1(2), 7, ':'); 
+        vline( r, 2);
+            pctile_r = 1 - (sum(nullrlat<r)+sum(nullrlat ==r)*0.5)/it;
+        xlabel('r')
+        title({'Spatial autocorrelation', ...
+                                  ['percentile = ' num2str(pctile_r)]})
+ mysubplot(1,2,2)
+ [r2,p2] = f_mantel(squareform(pdist(log(g_matST+1))),  squareform(pdist(t_infoST(:,2))), 0, 10000);
+   h = histogram(nullrmon, 20); hold on
+      h.EdgeColor = 'w';
+        q1 = quantile(nullrmon, [ 0.5 0.95],2)
+        %vline(q1(1), 7, ':'); 
+        vline(q1(1), 7, '--');   vline(q1(2), 7, ':'); 
+        vline( r2, 2);
+          pctile_r =  (sum(nullrmon<r2)+sum(nullrmon ==r2)*0.5)/it;
+            xlabel('r')
+        title({'Temporal autocorrelation', ...
+                                  ['percentile = ' num2str(pctile_r)]})
+  mysubplot(1,2,0,{ 'Mantel test Guild-based communities'})     
